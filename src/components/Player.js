b405fa60ed83abed2f6dbaf5e02b15965731a0ca
bodo79 <rubinstein.elad@gmail.com>
2020-10-07 11:19:45 +0300

    mobile-optimization v1.0.6

--- src/components/Player.js
+++ src/components/Player.js
@@ -5,12 +5,11 @@ const Player = ({playing, onReady, url}) => {
 	return (
 		<div className='tc dib br1 ma2 grow bw2 shadow-5'>
 			<ReactPlayer 
-				width='250px' 
-				height='250px' 
+				width='100px' 
+				height='100px' 
 				url={url}
 				playing
 				onReady={onReady}
-				light='https://freepngimg.com/download/chuck_norris/37468-5-chuck-norris.png'/>
 			/>
 		</div>
 
--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Scroll = (props) => {
 	return (
-		<div style={{overflowY: 'scroll', border: '3px solid black', height: '220px'}}>
+		<div style={{overflowY: 'scroll', border: '3px solid black', height: '260px'}}>
 			{ props.children }
-
+			<img src='https://freepngimg.com/download/chuck_norris/37468-5-chuck-norris.png'/>
 		</div>
 	);
 };
