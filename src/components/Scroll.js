a02379c15a2952111c2922806002cc514fbe7875
bodo79 <rubinstein.elad@gmail.com>
2020-10-07 11:33:54 +0300

    Fonts&colors v1.0.7

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props) => {
 	return (
-		<div style={{overflowY: 'scroll', border: '3px solid black', height: '260px'}}>
+		<div style={{overflowY: 'scroll', border: '3px solid black', height: '320px'}}>
 			{ props.children }
 			<img src='https://freepngimg.com/download/chuck_norris/37468-5-chuck-norris.png'/>
 		</div>
