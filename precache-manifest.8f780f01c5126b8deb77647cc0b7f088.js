043fc2795308edca06d72958d8309e8343e4541f
bodo79 <rubinstein.elad@gmail.com>
2020-10-07 06:14:51 +0300

    Updates

---
+++ precache-manifest.8f780f01c5126b8deb77647cc0b7f088.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9e90c49db4c30139d67e68357150f39",
+    "url": "/Chuck_Norris/index.html"
+  },
+  {
+    "revision": "9def4cb3c2ceeefc9c5e",
+    "url": "/Chuck_Norris/static/css/2.f778aee8.chunk.css"
+  },
+  {
+    "revision": "2d319bfe1b79873949c0",
+    "url": "/Chuck_Norris/static/css/main.2a163bf1.chunk.css"
+  },
+  {
+    "revision": "9def4cb3c2ceeefc9c5e",
+    "url": "/Chuck_Norris/static/js/2.1ccb235d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Chuck_Norris/static/js/2.1ccb235d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d319bfe1b79873949c0",
+    "url": "/Chuck_Norris/static/js/main.ba039c20.chunk.js"
+  },
+  {
+    "revision": "0970bc2eceeabde9ce9b",
+    "url": "/Chuck_Norris/static/js/runtime-main.44368f65.js"
+  },
+  {
+    "revision": "ccb7a958f18298a2f44b64855e39fd71",
+    "url": "/Chuck_Norris/static/media/SansitaSwashed-Light.ccb7a958.ttf"
+  }
+]);
